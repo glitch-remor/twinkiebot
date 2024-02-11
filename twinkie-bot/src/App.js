3230da40bcec407afe9c275d6798958f776473e3
reagan <r.morganeliz@gmail.com>
2024-02-11 01:01:30 -0600

    added route path to main and sub pages

--- twinkie-bot/src/App.js
+++ twinkie-bot/src/App.js
@@ -5,22 +5,14 @@ import SubPage from './SubPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/subpage1" element={<SubPage />} />
+        <Route path="/subpage2" element={<SubPage />} />
+        <Route path="/subpage3" element={<SubPage />} />
+      </Routes>
+    </Router>
   );
 }
 
